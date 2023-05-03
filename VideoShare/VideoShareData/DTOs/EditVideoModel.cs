07197ab760cd7b2ccf1d814a5615a7ec784a1ee7
lazkmet <lazkmet@gmail.com>
2023-05-03 01:16:29 -0400

    Reformat + Home Page
    Started development on Home Page
    Realized I need YT Video ID, not link - curse you database reworks :(
    Moved shared Blazor components into separate directory from other shared items

---
+++ VideoShare/VideoShareData/DTOs/EditVideoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VideoShareData.DTOs
+{
+    internal class EditVideoModel
+    {
+        //Regex for video url:
+         //var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        /*var match = url.match(regExp);
+      if (match && match[2].length == 11) {
+         var id = match[2];
+        var path = 'http://img.youtube.com/vi/' + id + '/0.jpg';
+         $(this).css('background-image', 'url(' + path + ')');*/
+    }
+}
