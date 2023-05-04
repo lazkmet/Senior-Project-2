138bb8bfcbcb96bef77ae7023a9610a89c476b31
lazkmet <lazkmet@gmail.com>
2023-05-04 17:47:35 -0400

    My Courses - Progress
    Added a form to capture a Course Code
    Migrated parameters to a Model DTO (couldn't skip that step)
    Did a bit of fiddling with the CSS, moved height and width to a better-defined parameter-passing object for components (did not update past reusables to use this parameter yet)

---
+++ VideoShare/VideoShareData/DTOs/CourseCodeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoShareData.DTOs
+{
+    public class CourseCodeModel
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z0-9]*$", ErrorMessage = "Invalid Characters Present")]
+        [MaxLength(6)]
+        [MinLength(6)]
+        public string enteredCode { get; set; }
+    }
+}
---
+++ VideoShare/VideoShareData/DTOs/StyleProperties.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoShareData.DTOs
+{
+    public class StyleProperties
+    {
+        public string? Height { get; set; }
+        public string? Width { get; set; }
+        public string? MaxHeight { get; set; }
+        public string? MaxWidth { get; set; }
+
+        public string GetStyle() {
+            string styleString = "";
+            if (!String.IsNullOrWhiteSpace(Height)) {
+                styleString += "height:" + Height + ";";
+            }
+            if (!String.IsNullOrWhiteSpace(Width))
+            {
+                styleString += "width:" + Width + ";";
+            }
+            if (!String.IsNullOrWhiteSpace(MaxHeight))
+            {
+                styleString += "max-height:" + MaxHeight + ";";
+            }
+            if (!String.IsNullOrWhiteSpace(MaxWidth))
+            {
+                styleString += "max-width:" + MaxWidth + ";";
+            }
+            return styleString;
+        }
+    }
+}
