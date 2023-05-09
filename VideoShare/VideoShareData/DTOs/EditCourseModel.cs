d0f28b93cb55168d6a46e02c5a8c85c06525d96e
lazkmet <lazkmet@gmail.com>
2023-05-09 01:00:22 -0400

    Created Edit Data Models for Video and Course
    I'll test it later

---
+++ VideoShare/VideoShareData/DTOs/EditCourseModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+using VideoShareData.Enums;
+
+namespace VideoShareData.DTOs
+{
+    public class EditCourseModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string CourseName { get; set; } = "";
+        [MaxLength(500)]
+        public string CourseDescription { get; set; } = "";
+        [Required]
+        public LessonLimitType LessonLimitType { get; set; } = LessonLimitType.None;
+        [Required]
+        [MustHaveOneElement(ErrorMessage = "Course must have at least one video.")]
+        public List<EditVideoModel> videos = new List<EditVideoModel>();
+        public List<EditVideoModel> deletedVideos = new List<EditVideoModel>();
+    }
+    //From https://stackoverflow.com/questions/13361500/array-must-contain-1-element
+    public class MustHaveOneElementAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var list = value as IList;
+            if (list != null)
+            {
+                return list.Count > 0;
+            }
+            return false;
+        }
+    }
+}
--- VideoShare/VideoShareData/DTOs/EditVideoModel.cs
+++ VideoShare/VideoShareData/DTOs/EditVideoModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,14 +9,44 @@ using System.Threading.Tasks;
 
 namespace VideoShareData.DTOs
 {
-    internal class EditVideoModel
+    public abstract class EditVideoModel
     {
-        //Regex for video url:
-         //var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-        /*var match = url.match(regExp);
-      if (match && match[2].length == 11) {
-         var id = match[2];
-        var path = 'http://img.youtube.com/vi/' + id + '/0.jpg';
-         $(this).css('background-image', 'url(' + path + ')');*/
+        [Required]
+        public int VideoId { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Video Title cannot be longer than 50 characters.")]
+        public string VideoTitle { get; set; } = "";
+        [Required]
+        [MaxLength(300, ErrorMessage = "Video Title cannot be longer than 300 characters.")]
+        public string VideoDescription { get; set; } = "";
+        //public List<AttachmentModel> {get;set;} = new List<AttachmentModel>();
     }
+    public class YouTubeEditVideoModel : EditVideoModel {
+        [Required]
+        [RegularExpression(@"^.* (youtu\.be\/| v\/| u\/\w\/| embed\/| watch\?v =|\&v =)([^#\&\?]*).*", ErrorMessage = "Invalid Youtube URL.")]
+        public string YouTubeURL { get; set; }
+        public bool UseYTDescription { get; set; } = false;
+        [Required]
+        public string? YouTubeID { get; set; }
+        public void GetIDFromURL() {
+            //Youtube URLs follow a specific pattern, with an 11-character video ID
+            if (string.IsNullOrEmpty(YouTubeURL)) { return; }
+            Regex URLRegex = new Regex(@"^.* (youtu\.be\/| v\/| u\/\w\/| embed\/| watch\?v =|\&v =)([^#\&\?]*).*");
+            var match = URLRegex.Match(YouTubeURL);
+            if (match.Success) {
+                var groups = match.Groups;
+                if (groups.Count > 2 && groups[2].Length == 11) {
+                    YouTubeID = groups[2].Value;
+                    return;
+                }
+            }
+            YouTubeID = null;
+            return;
+        }
+    }
+    /*
+    public class UploadedEditVideoModel : EditVideoModel {
+        TO DO
+    }
+    */
 }
